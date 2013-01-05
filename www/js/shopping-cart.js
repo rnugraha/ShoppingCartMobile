5a8eafa2f856f928244c121d461f50672b79138d
Riza Nugraha <rnugraha@gmail.com>
2013-01-05 20:42:50 +0100

    init

---
+++ www/js/shopping-cart.js
@@ -0,0 +1,13 @@
+//
+// A demo of simple Shopping Basket application
+// using combination of Backbone and jQuery-UI Mobile.
+//
+// User: rnugraha
+// Date: 1/5/13
+// Time: 8:20 PM
+//
+
+
+$(document).ready(function() {
+
+});
